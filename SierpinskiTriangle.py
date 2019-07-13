0c0ca12701212dfa23667f7ba735f7ec7b600b0a
wytskeb <wytske@utreg.net>
2019-07-13 14:34:53 +0200

    code omschrijven naar python

--- SierpinskiTriangle.py
+++ SierpinskiTriangle.py
@@ -6,21 +6,21 @@ from tkinter import messagebox
 
 top = Tk()
 
-dim = 1500
+dim = 400
 minSize = 4
 
-def drawGasket(x, y, dim, collerR, collerB, collerY):
+def drawGasket(x, y, dim, collerR, collerG, collerB):
     if dim <= minSize:
 #       stroke = collerR, collerB, collerY
-        fillcolor = '#' + str(hex(int(collerR)))[2:3] + str(hex(int(collerB)))[2:3] + str(hex(int(collerY)))[2:3]
+        fillcolor = '#' + str(hex(int(collerR/28)))[2:3] + str(hex(int(collerG/28)))[2:3] + str(hex(int(collerB/28)))[2:3]
         print(fillcolor)
         points = [x+dim/2, y, x, dim+y, dim+x, dim+y]
         poly = C.create_polygon(points, outline=fillcolor, fill=fillcolor, width=0)
     else:
         newDim = dim / 2
-        drawGasket(x + dim/4, y, newDim, collerR, collerB + newDim, collerY + newDim/100)
-        drawGasket(x, y + newDim, newDim, collerR + newDim/100, collerB, collerY + newDim)
-        drawGasket(x + newDim, y + newDim, newDim, collerR + newDim, collerB + newDim/100, collerY)
+        drawGasket(x + dim/4, y, newDim, collerR, collerG + newDim, collerB + newDim/100)
+        drawGasket(x, y + newDim, newDim, collerR + newDim/100, collerG, collerB + newDim)
+        drawGasket(x + newDim, y + newDim, newDim, collerR + newDim, collerG + newDim/100, collerB)
 
 
 def draw():
